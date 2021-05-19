807c592feeaf0fa7e9a53fc1674e05910f4b9306
DuBrowski <krdubray@comcast.net>
2021-05-19 11:49:27 -0500

    completed basic posting functionality, added CORS to posts and comment services

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
+import PostCreate from "./components/PostCreate";
+import PostList from "./components/PostList";
 
-export default () => {
-  return <div>Blog</div>;
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
 };
+
+export default App;
